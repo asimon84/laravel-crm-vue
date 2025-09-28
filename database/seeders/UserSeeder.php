77ce03d162db5e7eab97f4b8dea9127d72d4d9eb
asimon84 <ajsimon4@yahoo.com>
2025-09-27 20:06:18 -0700

    User Seeder added

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('test1234'),
+            'remember_token' => Str::random(100)
+        ]);
+
+        User::factory(10)->create();
+    }
+}
