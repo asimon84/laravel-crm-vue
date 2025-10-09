b01c27085b54d207a565338bce5afd4d0ef8ac6f
asimon84 <ajsimon4@yahoo.com>
2025-10-09 11:23:42 -0700

    Controller updates

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    /**
+     * Show the view for the Dashboard page
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index(Request $request) {
+//        $chartData = Record::getRecordsLastXDays(7);
+        $chartData = [];
+
+        return Inertia::render('Dashboard', compact('chartData'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RecordController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -7,9 +9,17 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get('dashboard', function () {
+//    return Inertia::render('Dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+//    Route::get('/records', [RecordController::class, 'index'])->name('chart-records');
+//    Route::get('/record/{id}', [RecordController::class, 'show'])->name('modal-record.show');
+//    Route::post('/record/{id}', [RecordController::class, 'edit'])->name('modal-record.edit');
+//    Route::delete('/record/{id}', [RecordController::class, 'delete'])->name('modal-record.delete');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
