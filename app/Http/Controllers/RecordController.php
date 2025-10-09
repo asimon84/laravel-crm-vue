18b08b5aafa8fa07d2b508c5eea11d2cd5c097e3
asimon84 <ajsimon4@yahoo.com>
2025-10-09 11:50:02 -0700

    yajra datatables added, record model updates

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -2,9 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Record;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class RecordController extends Controller
 {
+    /**
+     * Get data for records table on Dashboard page
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request) {
+        return DataTables::of(Record::all())
+            ->addIndexColumn()
+            ->addColumn('action', function($row){
+                $buttons = '<div style="width: 130px;">';
 
+                $buttons .= '<button type="button" class="btn btn-info view-record" data-bs-toggle="modal" data-bs-target="#recordModal" data-id='.$row->id.'><i class="bi bi-search"></i></button>';
+                $buttons .= '<button type="button" class="btn btn-success edit-record" data-bs-toggle="modal" data-bs-target="#recordModal" data-id='.$row->id.'><i class="bi bi-pencil"></i></button>';
+                $buttons .= '<button type="button" class="btn btn-danger delete-record" data-id='.$row->id.'><i class="bi bi-trash"></i></button>';
+
+                $buttons .= '</div>';
+
+                return $buttons;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+
+    /**
+     * Get data for record modal on Dashboard page
+     *
+     * @param Request $request
+     * @param int $id
+     *
+     * @return mixed
+     */
+    public function show(Request $request, int $id) {
+        return Record::find($id)->toJSON();
+    }
+
+    /**
+     * Update a record and return success or failure
+     *
+     * @param Request $request
+     * @param int $id
+     *
+     * @return bool
+     */
+    public function edit(Request $request, int $id):bool {
+        $record = Record::find($id);
+
+        $record->string = $request->input('string');
+        $record->text = $request->input('text');
+        $record->json = $request->input('json');
+        $record->boolean = $request->input('boolean');
+        $record->integer = $request->input('integer');
+        $record->float = $request->input('float');
+
+        return $record->save();
+    }
+
+    /**
+     * Delete a record and return success or failure
+     *
+     * @param Request $request
+     * @param int $id
+     *
+     * @return bool
+     */
+    public function delete(Request $request, int $id):bool {
+        return (bool) Record::destroy($id);
+    }
 }
