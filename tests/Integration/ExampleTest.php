69ea548e587a5a12401e5b9a9cda2c9f3c2ddea6
asimon84 <ajsimon4@yahoo.com>
2025-11-10 16:16:39 -0800

    tests updated

---
+++ tests/Integration/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Integration;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
